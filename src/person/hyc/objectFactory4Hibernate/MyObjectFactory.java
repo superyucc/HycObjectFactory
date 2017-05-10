47873bacacf2cf05ee286f5e8a8b7740035cc9d2
huangyc <1269780621@qq.com>
2017-05-10 17:14:28 +0800

    MyObjectFactory加了方法

--- src/person/hyc/objectFactory4Hibernate/MyObjectFactory.java
+++ src/person/hyc/objectFactory4Hibernate/MyObjectFactory.java
@@ -1,14 +1,18 @@
 package person.hyc.objectFactory4Hibernate;
 
+import java.util.List;
+
 public interface MyObjectFactory {
 	 
-	<T extends BaseEntityObject>T get(Class<T> clazz,int id);
+	<T extends BaseEntityObject> T get(Class<T> clazz,int id);
 	
-	<T>T create();
+	<T extends BaseEntityObject> T create();
 	
 	//Session getHibernateSession();
 	
 	void delete(BaseEntityObject entity);
 	
-	<T>T criteria(Class<T> clazz);//只负责放入objectFactory
+	<T extends BaseCriteria> T criteria(Class<T> clazz);//只负责放入objectFactory
+	
+	<T extends BaseEntityObject> List<T> all(Class<T> clazz);
 }
